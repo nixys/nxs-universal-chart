62932d23c9ce1c6eec4e76c965aaf85a8a0ad274
Evgenii Tereshkov <evg.krsk@gmail.com>
2022-04-10 23:29:56 +0700

    Improve test script

--- charts/universal-chart/testit.sh
+++ charts/universal-chart/testit.sh
@@ -1,14 +1,17 @@
-#!/bin/sh -eu
-mkdir -p tests
+#!/bin/sh -eux
+dname=$(dirname $0)
+tests="$dname/tests"
+rm -rf --one-file-system $tests
+mkdir -p $tests
 echo "Testing defaults"
-helm template test . > tests/default.yaml
-diff -u results/default.yaml tests/default.yaml
+helm template test $dname > $tests/default.yaml
+diff -u $dname/results/default.yaml $tests/default.yaml
 echo "OK"
-for i in samples/*
+for i in $dname/samples/*
 do
   echo "Testing $i"
   n=$(basename $i)
-  helm template test . --values $i |egrep -v '^ +helm.sh/chart:' > tests/$n
-  diff -u results/$n tests/$n
+  helm template test $dname --values $i |egrep -v '^ +helm.sh/chart:' > $tests/$n
+  diff -u $dname/results/$n $tests/$n
   echo "OK"
 done
